5684822fadac2052168a9b28871a40afb3b58775
FanOfSnake <shaukatk@list.ru>
2021-04-17 23:59:24 +0300

    added Query class (query root type)

---
+++ Query.cs
@@ -0,0 +1,16 @@
+﻿using GraphqlTest.Data;
+using HotChocolate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphqlTest
+{
+    public class Query
+    {
+        public IQueryable<Speaker> GetSpeakers([Service] ApplicationDbContext context)
+            => context.Speakers;
+
+    }
+}
