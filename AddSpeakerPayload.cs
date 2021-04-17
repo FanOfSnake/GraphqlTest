6dd19a56764d5f2c54997ad2da13beca08a95ae8
FanOfSnake <shaukatk@list.ru>
2021-04-18 00:11:56 +0300

    added addSpeakerPayload  for mutation

---
+++ AddSpeakerPayload.cs
@@ -0,0 +1,18 @@
+﻿using GraphqlTest.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphqlTest
+{
+    public class AddSpeakerPayload
+    {
+        public AddSpeakerPayload(Speaker speaker)
+        {
+            Speaker = speaker;
+        }
+
+        public Speaker Speaker { get; }
+    }
+}
