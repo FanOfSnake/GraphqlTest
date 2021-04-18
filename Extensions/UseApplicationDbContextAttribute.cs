2d4664a543764af901a091fd950297f4af8b7377
FanOfSnake <shaukatk@list.ru>
2021-04-18 12:34:04 +0300

    added so-called descriptor attribute which allows us to wrap graphql config code into attributes

---
+++ Extensions/UseApplicationDbContextAttribute.cs
@@ -0,0 +1,19 @@
+﻿using GraphqlTest.Data;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+using System.Reflection;
+
+namespace GraphqlTest.Extensions
+{
+    public class UseApplicationDbContextAttribute : ObjectFieldDescriptorAttribute
+    {
+        public override void OnConfigure(
+            IDescriptorContext context,
+            IObjectFieldDescriptor descriptor,
+            MemberInfo member
+            )
+        {
+            descriptor.UseDbContext<ApplicationDbContext>();
+        }
+    }
+}
