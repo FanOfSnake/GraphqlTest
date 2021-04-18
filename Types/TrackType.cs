fedc1cb5fc307ca05c6dd9bbc41d951d1340a1f8
FanOfSnake <shaukatk@list.ru>
2021-04-18 21:27:58 +0300

    added tracktype

---
+++ Types/TrackType.cs
@@ -0,0 +1,43 @@
+﻿using GraphqlTest.Data;
+using GraphqlTest.DataLoader;
+using HotChocolate;
+using HotChocolate.Resolvers;
+using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GraphqlTest.Types
+{
+    public class TrackType: ObjectType<Track>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Track> descriptor)
+        {
+            descriptor
+                .ImplementsNode()
+                .IdField(t => t.Id)
+                .ResolveNode((ctx, id) =>
+                    ctx.DataLoader<TrackByIdDataLoader>().LoadAsync(id,ctx.RequestAborted));
+        }
+
+        private class TrackResolvers
+        {
+            public async Task<IEnumerable<Session>> GetSessionsAsync(
+                Track track,
+                [ScopedService] ApplicationDbContext dbContext,
+                SessionByIdDataLoader sessionById,
+                CancellationToken cancellationToken)
+            {
+                int[] sessionsIds = await dbContext.Sessions
+                    .Where(s => s.Id == track.Id)
+                    .Select(s => s.Id)
+                    .ToArrayAsync();
+
+                return await sessionById.LoadAsync(sessionsIds,cancellationToken);
+            }
+        }
+    }
+}
