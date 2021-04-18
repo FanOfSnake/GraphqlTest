419adb79187f0e4fb6142fa7d63b047ffb210b3b
FanOfSnake <shaukatk@list.ru>
2021-04-18 13:57:19 +0300

    edited query class to use getiing by id methd

--- Query.cs
+++ Query.cs
@@ -1,8 +1,10 @@
 ﻿using GraphqlTest.Data;
+using GraphqlTest.DataLoader;
 using GraphqlTest.Extensions;
 using HotChocolate;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GraphqlTest
@@ -12,5 +14,12 @@ namespace GraphqlTest
         [UseApplicationDbContext]
         public Task<List<Speaker>> GetSpeakers([ScopedService] ApplicationDbContext context)
             => context.Speakers.ToListAsync();
+
+        public Task<Speaker> GetSpeakerAsync(
+            int id,
+            SpeakerByIdDataLoader dataLoader,
+            CancellationToken cancellationToken)
+            => dataLoader.LoadAsync(id, cancellationToken);
+
     }
 }
